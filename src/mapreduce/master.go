365540acf83ca07190f1d2591381e751c401d3b1
klbxg <liweiwei0404@gmail.com>
2015-06-25 22:11:21 -0400

    lab1-2

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -30,5 +30,78 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	var mapChannel, reduceChannel = make(chan bool, mr.nMap), make(chan bool, mr.nReduce)
+
+	for i := 0; i < mr.nMap; i++ {
+		go func (jobN int) {
+			var worker string
+			var jobArgs DoJobArgs
+			var reply DoJobReply
+			jobArgs.File = mr.file
+			jobArgs.Operation = Map
+			jobArgs.JobNumber = jobN
+			jobArgs.NumOtherPhase = mr.nReduce
+			for {
+				select {
+				case worker = <- mr.registerChannel:
+					call(worker, "Worker.DoJob", &jobArgs, &reply)
+					if (reply.OK) {
+						mapChannel <- true
+						mr.idleChannel <- worker
+						return
+					}
+				case worker = <- mr.idleChannel:
+					call(worker, "Worker.DoJob", &jobArgs, &reply)
+					if (reply.OK) {
+						mapChannel <- true
+						mr.idleChannel <- worker
+						return
+					}
+				}
+			}
+		}(i)
+	}
+
+	for i := 0; i < mr.nMap; i++ {
+		<- mapChannel
+	}
+	fmt.Println("Map is Done")
+
+	for i := 0; i < mr.nReduce; i++ {
+		go func (jobN int) {
+			var worker string
+			var jobArgs DoJobArgs
+			var reply DoJobReply
+			jobArgs.File = mr.file
+			jobArgs.Operation = Reduce
+			jobArgs.JobNumber = jobN
+			jobArgs.NumOtherPhase = mr.nMap
+			for {
+				select {
+				case worker = <- mr.registerChannel:
+					call(worker, "Worker.DoJob", jobArgs, &reply)
+					if (reply.OK) {
+						reduceChannel <- true
+						mr.idleChannel <- worker
+						return
+					}
+				case worker = <- mr.idleChannel:
+					call(worker, "Worker.DoJob", jobArgs, &reply)
+					if (reply.OK) {
+						reduceChannel <- true
+						mr.idleChannel <- worker
+						return
+					}
+				}
+			}
+		}(i)
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+		<- reduceChannel
+	}
+
+	fmt.Println("Reduce is Done")
+
 	return mr.KillWorkers()
 }
